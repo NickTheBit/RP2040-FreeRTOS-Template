253ec35c9e47c176fc8d1597831b7d7fb3a7a540
NickTheBit <nikosgklu@gmail.com>
2023-05-25 21:10:37 +0200

    Added initial implementation of hardware abstraction for GPIO.

---
+++ Src/HAL/GPIOPinInterface/RP2040/RP2040_GPIOPin.hh
@@ -0,0 +1,20 @@
+/*********************************************
+ * @brief Hardware specific driver for the
+ * GPIO pins of the RP2040.
+ * @author Nikolaos Xenofon Gkloumpos
+ * @date 25/05/2023
+ *********************************************/
+
+#include "HAL/GPIOPinInterface/GPIOPinInterface.hh"
+#include "main.h"
+
+class RP2040_GPIOPin : public HAL::GPIOPinInterface {
+  private:
+	uint8_t pin = 0;
+
+  public:
+	explicit RP2040_GPIOPin(uint8_t GPIOPin);
+	~RP2040_GPIOPin() = default;
+	void write(HAL::GPIO_STATE) override;
+	HAL::GPIO_STATE read() override;
+};
--- Src/PrimaryTask/PrimaryTask.cpp
+++ Src/PrimaryTask/PrimaryTask.cpp
@@ -7,17 +7,18 @@
 
 #include "PrimaryTask.hh"
 #include "main/main.h"
+#include "HAL/GPIOPinInterface/GPIOPinInterface.hh"
+#include "HAL/GPIOPinInterface/RP2040/RP2040_GPIOPin.hh"
 
 void primaryTask(void *arguments) {
 	// Configure the Pico's on-board LED as a heartbeat indicator.
-	gpio_init(PICO_DEFAULT_LED_PIN);
-	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+	auto heartbeatPin = RP2040_GPIOPin(PICO_DEFAULT_LED_PIN);
 
 	while (true) {
         // Starting a heartbeat to indicate proper initialization.
-		gpio_put(PICO_DEFAULT_LED_PIN, 0);
+		heartbeatPin.write(HAL::GPIO_STATE::LOW);
 		vTaskDelay(1000);
-		gpio_put(PICO_DEFAULT_LED_PIN, 1);
+		heartbeatPin.write(HAL::GPIO_STATE::HIGH);
 		vTaskDelay(50);
 	}
 }
