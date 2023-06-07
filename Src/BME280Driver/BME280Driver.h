75ed9c803b1b564afa47ef89193d1d6daf8fce7a
NickTheBit <nikosgklu@gmail.com>
2023-06-07 21:21:09 +0200

    WIP, progress on BME280 driver

--- Src/BME280Driver/BME280Driver.h
+++ Src/BME280Driver/BME280Driver.h
@@ -6,6 +6,39 @@
 
 #pragma once
 
-class BME280Driver {
+#include "HAL/I2CInterface.hh"
+#include "BME280_defs.hh"
 
+struct filteredMeasurements {
+    double pressure;
+    double temperature;
+    double humidity;
+};
+
+struct rawMeasurements {
+    uint32_t pressure;
+    uint32_t temperature;
+    uint32_t humidity;
+};
+
+class BME280Driver {
+private:
+    I2CInterface &i2cBus;
+    uint8_t address;
+    struct filteredMeasurements currentMeasurements = {};
+
+    void readRegister(uint8_t registerAddress, uint8_t *returnedData, uint32_t length);
+    void writeRegister(uint8_t registerAddress, uint8_t *dataToWrite, uint32_t length);
+public:
+    BME280Driver(I2CInterface& i2cBus, uint8_t address);
+    ~BME280Driver() = default;
+
+    BME280::sensorPowerMode_t getSensorMode();
+    void setSensorMode(BME280::sensorPowerMode_t newState);
+    void softReset();
+    void getSensorData();
+
+    double getTemperature();
+    double getHumidity();
+    double getPressure();
 };
--- Src/HAL/I2CInterface.hh
+++ Src/HAL/I2CInterface.hh
@@ -10,6 +10,7 @@
 #include <cstdint>
 
 class I2CInterface {
+public:
     virtual void read(uint8_t address, uint8_t consecutiveBytes, uint8_t *outputArray) = 0;
     virtual void write(uint8_t startingAddress, uint8_t *payload, uint16_t payloadSize) = 0;
 };
