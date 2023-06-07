b37ff768160ed75e843e596165071527db49f004
NickTheBit <nikosgklu@gmail.com>
2023-06-07 21:23:30 +0200

    Template cleanup.

--- Src/BME280Driver/BME280Driver.cpp
+++
@@ -1,7 +0,0 @@
-/**********************************************
- * @brief  Bare-bone driver for the BME280 sensor.
- * @author Nikolaos Xenofon Gkloumpos
- * @date   6/1/23
- **********************************************/
-
-#include "BME280Driver.h"
